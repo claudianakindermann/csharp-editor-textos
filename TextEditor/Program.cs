9a826723069d3e36768ab0c6e23c55ce4864f384
Claudiana Kindermann <56567665+claudianakindermann@users.noreply.github.com>
2022-08-16 14:04:50 -0300

    text editor

---
+++ TextEditor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Selecione a opção desejada: ");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("0 - Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                case 0: System.Environment.Exit(0); break;
+            }
+        }
+        static void Abrir()
+        {
+
+        }
+
+        static void Editar()
+        {
+
+        }
+    }
+
+}
